c861e749055412a803bbf134c251ab51afea7896
aljt <alvinljt@gmail.com>
2020-12-17 17:53:59 +0800

    implemented 4.2.3

--- index.mjs
+++ index.mjs
@@ -1,41 +1,39 @@
 import db from './models/index.mjs';
 
-db.Category
-  .findOne({
-    where: {
-      name: process.argv[2]
-    }
-  })
-  .then((returnedCategory) => {
-    // Docs on .create
-    // https://sequelize.org/master/class/lib/model.js~Model.html#static-method-create
-    // Return statement returns the Promise returned by the final .then
-    return db.Item.create(
-      {
-        name: process.argv[3],
+// // get items in a category
+// const getData = async () => {
+//   try {
+//     const category = await db.Category.findOne({
+//       where: {
+//         name: [process.argv[2]],
+//       },
+//     });
+
+//     const categoryItems = await category.getItems();
+
+//     console.log(categoryItems);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// getData();
+
+// get category of an item
+const getData = async () => {
+  try {
+    const item = await db.Item.findOne({
+      where: {
+        name: [process.argv[2]],
       },
-      // {
-      //   // Return only the id column
-      //   returning: ['id']
-      // }
-    ).then((newItem) => {
-      // Associate newItem with returnedCategory using the setCategory
-      // method on newItem that Sequelize provides for us because of the
-      // belongsTo association we defined in models/index.mjs.
-      // This logic because less clunky after we introduce async/await syntax.
-      // Docs: https://sequelize.org/master/manual/assocs.html#-code-foo-belongsto-bar---code-
-      console.log('new item is ', newItem);
-      return newItem.setCategory(returnedCategory).then(() => {
-        return newItem;
-      });
     });
-    
-  }).then(result => {
 
-    console.log('success!!');
-    console.log(result);
+    const itemCategory = await item.getCategory();
 
-  }).catch(error => {
+    console.log(itemCategory);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    console.log(error)
-  });
+getData();
